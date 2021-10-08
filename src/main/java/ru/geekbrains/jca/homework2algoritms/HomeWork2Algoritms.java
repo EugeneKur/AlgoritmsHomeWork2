6626ea0f93594c0c72b169aff62d7c99516a39da
EugeneKur <ew.kurilov@gmail.com>
2021-10-08 11:00:58 +0300

    Commmmment

--- src/main/java/ru/geekbrains/jca/homework2algoritms/HomeWork2Algoritms.java
+++ src/main/java/ru/geekbrains/jca/homework2algoritms/HomeWork2Algoritms.java
@@ -1,4 +1,29 @@
 package ru.geekbrains.jca.homework2algoritms;
+import java.util.Arrays;
 
 public class HomeWork2Algoritms {
+    public static void main(String[] args) {
+
+        int amount = 5000;
+        Object[][] arr = new Object[amount][];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Notebook.getNotebook();
+
+        }
+        printdArray(arr);
+        System.out.println();
+        SelectionSort.sort(arr);
+        printdArray(arr);
+
+    }
+
+    static void  printdArray(Object[][] arr) { // метод вывода 2д массива
+        for (int y = 0; y < arr.length; y++) {
+            for (int x = 0; x < arr[y].length; x++) {
+                System.out.print(arr[y][x] + "  ");
+            }
+            System.out.println();
+        }
+
+    }
 }
